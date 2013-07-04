40c4b0de7815cfb7385442e6e8bff80f6178cd65
YukonAppleGeek <yukonvinecki@gmail.com>
2013-07-03 23:32:28 -0700

    Implement fuctionalty, version 1.0.0

--- src/main/java/me/yukonapplegeek/festivearrows/FestiveArrows.java
+++ src/main/java/me/yukonapplegeek/festivearrows/FestiveArrows.java
@@ -1,6 +1,16 @@
 package me.yukonapplegeek.festivearrows;
 
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+import org.bukkit.FireworkEffect.Type;
+import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Firework;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FestiveArrows extends JavaPlugin implements Listener {
@@ -13,4 +23,19 @@ public class FestiveArrows extends JavaPlugin implements Listener {
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
     }
+    
+    @EventHandler
+    public void onBowShoot(EntityShootBowEvent event) {
+        FireworkMeta fireworkMeta = (FireworkMeta) (new ItemStack(Material.FIREWORK)).getItemMeta();
+        Firework firework = (Firework) event.getProjectile().getLocation().getWorld().spawnEntity(event.getProjectile().getLocation(), EntityType.FIREWORK);
+
+        fireworkMeta.addEffect(FireworkEffect.builder()
+                                             .with(Type.BURST)
+                                             .withColor(Color.RED).withColor(Color.WHITE).withColor(Color.BLUE)
+                                             .withTrail()
+                                             .build());
+        firework.setFireworkMeta(fireworkMeta);
+        event.getProjectile().setPassenger(firework);
+    }
+    
 }
